a91bc36c2fad31eb8dd3cd83e976fd5603686956
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-05-03 18:22:53 +0200

    docs/tutorial/smenu.py: autopep8 changes

--- docs/tutorial/smenu.py
+++ docs/tutorial/smenu.py
@@ -2,6 +2,7 @@ import urwid
 
 choices = u'Chapman Cleese Gilliam Idle Jones Palin'.split()
 
+
 def menu(title, choices):
     body = [urwid.Text(title), urwid.Divider()]
     for c in choices:
@@ -10,19 +11,21 @@ def menu(title, choices):
         body.append(urwid.AttrMap(button, None, focus_map='reversed'))
     return urwid.ListBox(urwid.SimpleFocusListWalker(body))
 
+
 def item_chosen(button, choice):
     response = urwid.Text([u'You chose ', choice, u'\n'])
     done = urwid.Button(u'Ok')
     urwid.connect_signal(done, 'click', exit_program)
     main.original_widget = urwid.Filler(urwid.Pile([response,
-        urwid.AttrMap(done, None, focus_map='reversed')]))
+                                                    urwid.AttrMap(done, None, focus_map='reversed')]))
+
 
 def exit_program(button):
     raise urwid.ExitMainLoop()
 
 main = urwid.Padding(menu(u'Pythons', choices), left=2, right=2)
 top = urwid.Overlay(main, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
-    align='center', width=('relative', 60),
-    valign='middle', height=('relative', 60),
-    min_width=20, min_height=9)
+                    align='center', width=('relative', 60),
+                    valign='middle', height=('relative', 60),
+                    min_width=20, min_height=9)
 urwid.MainLoop(top, palette=[('reversed', 'standout', '')]).run()

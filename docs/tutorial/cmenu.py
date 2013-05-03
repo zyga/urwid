afde27e9274c436ae13c80cb0df6b7c07348ab07
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-05-03 18:22:51 +0200

    docs/tutorial/cmenu.py: autopep8 changes

--- docs/tutorial/cmenu.py
+++ docs/tutorial/cmenu.py
@@ -1,25 +1,31 @@
 import urwid
 
+
 def menu_button(caption, callback):
     button = urwid.Button(caption)
     urwid.connect_signal(button, 'click', callback)
     return urwid.AttrMap(button, None, focus_map='reversed')
 
+
 def sub_menu(caption, choices):
     contents = menu(caption, choices)
+
     def open_menu(button):
         return top.open_box(contents)
     return menu_button([caption, u'...'], open_menu)
+
 
 def menu(title, choices):
     body = [urwid.Text(title), urwid.Divider()]
     body.extend(choices)
     return urwid.ListBox(urwid.SimpleFocusListWalker(body))
 
+
 def item_chosen(button):
     response = urwid.Text([u'You chose ', button.label, u'\n'])
     done = menu_button(u'Ok', exit_program)
     top.open_box(urwid.Filler(urwid.Pile([response, done])))
+
 
 def exit_program(button):
     raise urwid.ExitMainLoop()
@@ -39,6 +45,7 @@ menu_top = menu(u'Main Menu', [
     ]),
 ])
 
+
 class CascadingBoxes(urwid.WidgetPlaceholder):
     max_box_levels = 4
 
@@ -49,14 +56,15 @@ class CascadingBoxes(urwid.WidgetPlaceholder):
 
     def open_box(self, box):
         self.original_widget = urwid.Overlay(urwid.LineBox(box),
-            self.original_widget,
-            align='center', width=('relative', 80),
-            valign='middle', height=('relative', 80),
-            min_width=24, min_height=8,
-            left=self.box_level * 3,
-            right=(self.max_box_levels - self.box_level - 1) * 3,
-            top=self.box_level * 2,
-            bottom=(self.max_box_levels - self.box_level - 1) * 2)
+                                             self.original_widget,
+                                             align='center', width=('relative', 80),
+                                             valign='middle', height=('relative', 80),
+                                             min_width=24, min_height=8,
+                                             left=self.box_level * 3,
+                                             right=(
+                                             self.max_box_levels - self.box_level - 1) * 3,
+                                             top=self.box_level * 2,
+                                             bottom=(self.max_box_levels - self.box_level - 1) * 2)
         self.box_level += 1
 
     def keypress(self, size, key):

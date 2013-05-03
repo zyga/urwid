62e0dfccdb1cb12cfcd9c98237d5b67f45e32d2c
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-05-03 18:22:51 +0200

    docs/tutorial/attr.py: autopep8 changes

--- docs/tutorial/attr.py
+++ docs/tutorial/attr.py
@@ -1,5 +1,6 @@
 import urwid
 
+
 def exit_on_q(key):
     if key in ('q', 'Q'):
         raise urwid.ExitMainLoop()
@@ -7,7 +8,7 @@ def exit_on_q(key):
 palette = [
     ('banner', 'black', 'light gray'),
     ('streak', 'black', 'dark red'),
-    ('bg', 'black', 'dark blue'),]
+    ('bg', 'black', 'dark blue'), ]
 
 txt = urwid.Text(('banner', u" Hello World "), align='center')
 map1 = urwid.AttrMap(txt, 'streak')

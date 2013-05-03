95fbf368cfe84592365505e4caa573fd330ebfad
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-05-03 18:22:53 +0200

    docs/tutorial/sig.py: autopep8 changes

--- docs/tutorial/sig.py
+++ docs/tutorial/sig.py
@@ -1,6 +1,6 @@
 import urwid
 
-palette = [('I say', 'default,bold', 'default', 'bold'),]
+palette = [('I say', 'default,bold', 'default', 'bold'), ]
 ask = urwid.Edit(('I say', u"What is your name?\n"))
 reply = urwid.Text(u"")
 button = urwid.Button(u'Exit')
@@ -8,8 +8,10 @@ div = urwid.Divider()
 pile = urwid.Pile([ask, div, reply, div, button])
 top = urwid.Filler(pile, valign='top')
 
+
 def on_ask_change(edit, new_edit_text):
     reply.set_text(('I say', u"Nice to meet you, %s" % new_edit_text))
+
 
 def on_exit_clicked(button):
     raise urwid.ExitMainLoop()
